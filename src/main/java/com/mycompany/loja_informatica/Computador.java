a25edc348d274b820269a4c829ec77c67b150d33
Eduardo Feitosa <eduardofeitosa19@icloud.com>
2022-10-12 16:40:47 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/loja_informatica/Computador.java
@@ -0,0 +1,36 @@
+package com.mycompany.loja_informatica;
+
+public class Computador {
+
+    private String hd;
+    private String processador;
+    private static String marca = "EduardoFeitosa";
+
+    public Computador() {
+    }
+
+    public String getHd() {
+        return hd;
+    }
+
+    public void setHd(String hd) {
+        this.hd = hd;
+    }
+
+    public String getProcessador() {
+        return processador;
+    }
+
+    public void setProcessador(String processador) {
+        this.processador = processador;
+    }
+
+    public static String getMarca() {
+        return marca;
+    }
+
+    public static void setMarca(String marca) {
+        Computador.marca = marca;
+    }
+
+}
---
+++ src/main/java/com/mycompany/loja_informatica/Loja_Informatica.java
@@ -0,0 +1,9 @@
+package com.mycompany.loja_informatica;
+
+
+public class Loja_Informatica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
